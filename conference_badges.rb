bc99eb53688ebbe720d290fec426286351a7ca5f
Stephen Courtier <steves0089@gmail.com>
2018-09-10 12:59:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each do |name|
-    "Hello, #{name}! You'll be assigned to room #{name.index + 1}!"
+  attendees.each_with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end 
 end
   
