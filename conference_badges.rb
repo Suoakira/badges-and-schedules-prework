80c5035c175157aaacfbb993797fa3e4bf23fb1b
Stephen Courtier <steves0089@gmail.com>
2018-09-10 12:55:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  badge_messeges = []
   attendees.each do |name|
     badge_messeges.push(badge_maker(name))
   end
+  badge_messeges
 end
   
